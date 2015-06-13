b4dd30dfe74158a59ebd0e97696641e5724f7e5b
sennes <sennes111762@gmail.com>
2015-06-13 15:18:55 +0800

    finish

---
+++ remove-duplicates-from-sorted-array/main.py
@@ -0,0 +1,14 @@
+__author__ = 'sen'
+
+nums = [1,1,2]
+def removeDuplicates(nums):
+    i = 0
+    while(i+1<=len(nums)-1):
+        if nums[i] == nums[i+1]:
+            del nums[i+1]
+        else:
+            i = i+1
+    return  len(nums)
+
+
+print removeDuplicates(nums)
